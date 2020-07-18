3202edb1dd848650a7d82369cd6f0dd1f1b1c8c8
BrandonKutivan <bskutivan@gmail.com>
2020-07-18 12:29:00 -0700

    added uplodadTransaction to idb.js file as well as event listener for reconnecting to network

--- public/js/idb.js
+++ public/js/idb.js
@@ -31,3 +31,44 @@ function saveRecord(record) {
     // add record to store with add method
     transactionObjectStore.add(record)
 }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                        'Content-type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+
+                transactionObjectStore.clear();
+
+                alert('All saved transactions have been submitted and cleared.');
+            })
+            .catch(err => {
+                console.log('uh oh');
+                console.log(err);
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
